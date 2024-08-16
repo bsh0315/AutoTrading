c428f7d1415768db044e7fd1e5c456525cca3fc5
bsh0315 <shbackrok@gmail.com>
2024-08-16 11:16:10 +0900

    계좌번호 불러오기 설정 완료

--- kiwoom/kiwoom/kiwoom.py
+++ kiwoom/kiwoom/kiwoom.py
@@ -1,5 +1,7 @@
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import *
+from config.errorCode import *
+
 class Kiwoom(QAxWidget):
     
     def __init__(self):
@@ -10,9 +12,15 @@ class Kiwoom(QAxWidget):
         self.login_event_loop = None
         ###############################
         
+        
+        ##### 변수 모음 ########
+        self.account_num = None # 내 계좌번호
+        #######################
+        
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConect()
+        self.get_account_info()
         
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOPENAPICtrl.1")
@@ -22,6 +30,7 @@ class Kiwoom(QAxWidget):
         
     def login_slot(self,errCode):
         print(errCode)
+        errors(errCode)
         
         self.login_event_loop.exit() # 키움 로그인 완료시에 이벤트 루프를 끊어줌
         
@@ -30,3 +39,12 @@ class Kiwoom(QAxWidget):
         
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
+        
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLogininfo(String)", "ACCNO")
+        
+        self.account_num = account_list.split(';')[0] 
+        #계좌가 여러개인 경우에 ;를 기준으로 나눠짐.
+        # [0]은 제일 처음의 계좌번호를 가져옴
+        
+        print(f"나의 보유 계좌번호 : {self.account_num}")
