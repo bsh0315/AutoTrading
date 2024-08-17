fde17c2a0bc813ca9506bec19a3a3f9fdbb89f7f
bsh0315 <shbackrok@gmail.com>
2024-08-17 23:27:42 +0900

    잔고 종목정보 불러오기-2

--- kiwoom/kiwoom/kiwoom.py
+++ kiwoom/kiwoom/kiwoom.py
@@ -10,8 +10,8 @@ class Kiwoom(QAxWidget):
         
         #### eventloop 모음 ###
         self.login_event_loop = None # 로그인 시
-        self.detail_account_info_event_loop = None # 예수금 TR요청 시 사용
-        self.detail_account_mystock_info_event_loop = None # 계좌평가잔고 요청시 사용
+        self.detail_account_info_event_loop = None # 예수금 TR(Transaction Request)요청 시 사용
+        self.detail_account_mystock_info_event_loop = QEventLoop() # 계좌평가잔고 요청시 사용
         ###############################
         
         
@@ -23,11 +23,10 @@ class Kiwoom(QAxWidget):
         #### 계좌관련 변수 #####
         self.use_money = 0 # 예수금
         self.use_money_percent = 0.7 # 예수금 중 실제로 매매에 사용할 비율
+        self.buy_stock_quantity = 10 
+        self.accout_stock_dict = None # 계좌 내 종목들의 딕셔너리
         ###########################
-        
-        
-        
-        
+
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConect()
@@ -93,7 +92,6 @@ class Kiwoom(QAxWidget):
         self.dynamicCall("CommRqData(QString, QString, int, QString)", "계좌평가잔고내역요청", "opw00018", 0, self.screen_my_info)
         
         # 요청 후 이벤트 루프 실행
-        self.detail_account_mystock_info_event_loop = QEventLoop()
         self.detail_account_mystock_info_event_loop.exec_()
         
     
@@ -103,10 +101,10 @@ class Kiwoom(QAxWidget):
         '''
         TR요청을 받는 변수 
         sScrNo : 스크린번호
-        sRQName : 내가 요청했을 때 지은 이름
+        sRQName : 내가  TR을 요청할 때 부여한 이름
         sTrCode : 요청id, tr코드
         sRecordName : 사용안함.
-        sPrevNext : 다음 페이지가 있는지
+        sPrevNext : 계좌 잔고에서 페이지당 최대 20개만 표기됨. 이를 넘어갈 경우 이 변수가 사용됨.
         '''
         
         if sRQName == "예수금상세현황요청":
@@ -114,7 +112,7 @@ class Kiwoom(QAxWidget):
             print(f"예수금 : {int(deposit)}")
             
             self.use_money = int(deposit) * self.use_money_percent
-            self.use_money /= 4
+            self.use_money /= self.buy_stock_quantity # 한 종목을 매수할 때 이용가능한 시드의 1/self.buy_stock_quantity만큼 매수가능
 
             
             avail_money = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "출금가능금액")
@@ -126,7 +124,8 @@ class Kiwoom(QAxWidget):
             
             # 이벤트 루프 종료
             self.detail_account_info_event_loop.exit() 
-            
+        
+        total_cnt = 0
         if sRQName == "계좌평가잔고내역요청":
             
             total_buy_money = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "총매입금액")
@@ -136,7 +135,39 @@ class Kiwoom(QAxWidget):
             total_profit_rate = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "총수익률(%)")
             total_profit_rate = float(total_profit_rate)
             print(f"총 수익률 : {total_profit_rate}")
-            '''
+            
+            
+            rows = self.dynamicCall("GetRepeatCnt(QString, QString)", sTrCode, sRQName) # 보유종목 수
+            # GetRepearCnt를 쓸때는 무조건 KOA studio의 TR에서 멀티데이터를 가져온다는 의미임.
+            # GetRepeatCnt 함수는 한번에 20개의 종목의 데이터만 불러올 수 있음.
+            
+            cnt = 0
+            for i in range(rows):
+                code  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목번호")
+                stock_nm  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목명")
+                stock_profit  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "평가손익")
+                stock_profit_rate  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "수익률(%)")
+                stock_quantity  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "보유수량")
+                stock_avail_sell  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매매가능수량")
+                stock_value  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "평가금액")
+                stock_position_rate  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "보유비중")
+                
+                code = code.split()[1:]
+                
+                if code in self.accout_stock_dict:
+                    pass
+                else:
+                    self.account_stock_dict.update({code : {}})
+                    
+                stock_nm = stock_nm.split()
+                stock_profit = int(stock_profit)
+                stock_profit_rate = float(stock_profit_rate)
+                stock_quantity = int(stock_quantity)
+                stock_avail_sell = int(stock_avail_sell)
+                stock_value = int(stock_value)
+                stock_position_rate = float(stock_position_rate)
+                
+                '''
                 #### 종목관련 변수 ######
                 code = None # 종목코드
                 stock_nm = None # 종목명
@@ -147,25 +178,26 @@ class Kiwoom(QAxWidget):
                 stock_value = None # 종목 평가금액
                 stock_position_rate = None # 전체 대비 보뮤비중
                 ##################
-            '''
-            
-            rows = self.dynamicCall("GetRepearCnt(QString, QString)", sTrCode, sRQName) # 보유종목 수
-            # GetRepearCnt를 쓸때는 무조건 KOA studio의 TR에서 멀티데이터를 가져온다는 의미임.
-            
-            cnt = 0 
-            print(f"보유종목 수 : {rows}")
-            for i in range(rows):
-                code  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "종목번호")
-                stock_nm  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "종목명")
-                stock_profit  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "평가손익")
-                stock_profit_rate  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "수익률(%)")
-                stock_quantity  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "보유수량")
-                stock_avail_sell  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "매매가능수량")
-                stock_value  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "평가금액")
-                stock_position_rate  = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "보유비중")
+                '''
                 
+                self.accout_stock_dict[code].update({"종목명" : stock_nm})
+                self.accout_stock_dict[code].update({"평가손익" : stock_profit})
+                self.accout_stock_dict[code].update({"수익률" : stock_profit_rate})
+                self.accout_stock_dict[code].update({"보유수량" : stock_quantity})
+                self.accout_stock_dict[code].update({"매매가능수량" : stock_avail_sell})
+                self.accout_stock_dict[code].update({"평가금액" : stock_value})
+                self.accout_stock_dict[code].update({"보유비중" : stock_position_rate})
                 
+                cnt += 1
             
-            self.detail_account_mystock_info_event_loop.exit()
-
+            print(f"계좌에 가지고 있는 종목 수 : {cnt}")
+            print(f"종목정보 : {self.accout_stock_dict}")
             
+            total_cnt += cnt
+            
+            if sPrevNext == 2:
+                self.detail_account_mystock(sPrevNext = "2")
+            else: 
+                self.detail_account_mystock_info_event_loop.exit()
+                
+        print(f"총 종목 수 : {total_cnt}")
